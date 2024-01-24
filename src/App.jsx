2155e90ed7524dbfdf0302b98d0b5236dcaa81b0
ashiknrhq04 <ashikrn.hq04@gmail.com>
2024-01-24 22:51:50 +0600

    initital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+        <h1 className="underline text-red-600">Hello TailWind</h1>
+    </>
+  )
+}
+
+export default App
